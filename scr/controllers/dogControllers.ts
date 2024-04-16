33f47212579d55ae29fcbdef9460d58437279035
HarryGor128 <harry99128@gmail.com>
2024-04-16 23:11:37 +0800

    [API] Add dog record

--- scr/controllers/dogControllers.ts
+++ scr/controllers/dogControllers.ts
@@ -1,16 +1,42 @@
 import Koa from 'koa';
 import firebaseServices from '../services/firebaseServices';
+import Dog from '../types/Dog';
+import objTypeChecking from '../utils/objTypeChecking';
 
 const dogControllers = {
+    // Get all dogs info
     getAllDogsInfo: async (ctx: Koa.Context) => {
         const result = await firebaseServices.getCollection('dog');
 
         ctx.body = result ? result : [];
-    }, // Get all dogs info
-    addNewDogInfo: (ctx: Koa.Context) => {
-        const newRecord: Dog = ctx.request.body;
-        console.log('🚀 ~ file: dogControllers.ts:12 ~ newRecord:', newRecord);
-    }, // Add new dog
+    },
+
+    // Add new dog
+    addNewDogInfo: async (ctx: Koa.Context) => {
+        const newRecord = ctx.request.body as Dog;
+
+        if (objTypeChecking(newRecord, new Dog())) {
+            ctx.status = 400;
+        }
+
+        const nextId = await firebaseServices.getNextId('dog', 'id');
+        newRecord.id = nextId;
+        console.log(
+            '🚀 ~ file: dogControllers.ts:18 ~ addNewDogInfo: ~ newRecord:',
+            newRecord,
+        );
+
+        const result = await firebaseServices.addDoc('dog', nextId, newRecord);
+
+        if (result.result) {
+            ctx.status = 200;
+        } else {
+            ctx.status = 500;
+            ctx.message = result.msg;
+        }
+    },
+
+    // Get dog info by id
     getDogInfo: async (ctx: Koa.Context) => {
         const { id } = ctx.query;
         const result = await firebaseServices.getDoc('dog', id as string);
@@ -20,9 +46,13 @@ const dogControllers = {
         } else {
             ctx.status = 404;
         }
-    }, // Get dog info by id
-    updateDogInfo: (ctx: Koa.Context) => {}, // Update dog info by id
-    deleteDogInfo: (ctx: Koa.Context) => {}, // Delete dog info by id
+    },
+
+    // Update dog info by id
+    updateDogInfo: async (ctx: Koa.Context) => {},
+
+    // Delete dog info by id
+    deleteDogInfo: async (ctx: Koa.Context) => {},
 };
 
 export default dogControllers;
