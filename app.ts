bec37521b91ec1633d8b291e4903ff3c1f6ae4e2
harry.chan <harry.chan@speedylinkage.com>
2024-04-19 14:07:18 +0800

    Change to 3000

--- app.ts
+++ app.ts
@@ -18,5 +18,5 @@ authRoutes(router);
 koa.use(bodyParser());
 koa.use(router.routes());
 koa.use(logger());
-koa.listen(process.env.port);
+koa.listen(3000);
 // console.log('Debug link: http://localhost:3003/');
