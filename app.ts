7ea27c96d00bcc56921f80d935e69e2e7f429473
harry.chan <harry.chan@speedylinkage.com>
2024-04-19 14:43:36 +0800

    Change Port

--- app.ts
+++ app.ts
@@ -18,5 +18,5 @@ authRoutes(router);
 koa.use(bodyParser());
 koa.use(router.routes());
 koa.use(logger());
-koa.listen(3000);
+koa.listen(process.env.PORT);
 // console.log('Debug link: http://localhost:3003/');
